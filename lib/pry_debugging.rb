33b9f4fa0b77df6f97bade6343bdfd0e95b10ac7
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-22 18:32:22 +0000

    Automatically backed up by Learn

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -4,4 +4,19 @@ def plus_two(num)
 	end 
 
 
-	
+def my_all?(collection)
+  i= 0
+  while i < collection.length
+    all_bigger = true
+    yield collection[i]
+    i += 1
+  end
+  while i >collection.length
+    all_smaller = false
+    yield collection[i]
+    i += 1
+  end
+  all_smaller
+  all_bigger 
+  #binding.pry
+end
